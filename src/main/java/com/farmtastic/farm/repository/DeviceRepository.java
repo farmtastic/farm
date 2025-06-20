183638022ca2d8ed10325c2591b16f8df5e2e7cf
KIMDAB <kdabin111@naver.com>
2025-06-20 18:00:54 +0900

    ++ 코드 정리

--- src/main/java/com/farmtastic/farm/repository/DeviceRepository.java
+++ src/main/java/com/farmtastic/farm/repository/DeviceRepository.java
@@ -1,11 +1,9 @@
 package com.farmtastic.farm.repository;
 
-import com.farmtastic.farm.domain.AutomationRule;
+
 import com.farmtastic.farm.domain.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
 }
