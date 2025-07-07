02a8037069518d08f14a82f1352a65abbfc6a720
jimizip <9koorry9@gmail.com>
2025-07-07 14:45:47 +0900

    Feat[BE]: Gateway, yml 설정 파일

--- src/main/java/com/farmtastic/farm/config/MqttGateway.java
+++ src/main/java/com/farmtastic/farm/config/MqttGateway.java
@@ -1,4 +1,15 @@
 package com.farmtastic.farm.config;
 
-public class MqttGateway {
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+// 서비스 계층에서 MQTT 메시지를 쉽게 발행할 수 있도록 제공하는 게이트웨이 인터페이스
+// 다른 서비스에서 복잡한 채널 설정 없이 mqttGateway.sendToMqtt(topic, payload) 한 줄만으로 메시지를 보낼 수 있음.
+@MessagingGateway(defaultRequestChannel = "mqttOutboundChannel") // 메시지를 보낼 채널 지정
+public interface MqttGateway {
+
+    // 지정된 토픽으로 메시지(payload)를 보냄
+    void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, String payload);
+
 }
--- src/main/java/com/farmtastic/farm/config/MqttProperties.java
+++ src/main/java/com/farmtastic/farm/config/MqttProperties.java
@@ -1,4 +1,13 @@
 package com.farmtastic.farm.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "mqtt") // "mqtt"로 시작하는 설정을 매핑
 public class MqttProperties {
+    private String url;
+    private String clientId;
+    // private String username;
+    // private String password;
 }
