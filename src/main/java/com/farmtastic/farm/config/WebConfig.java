917df662f87da87d9739e9943001ca933ba65b37
sangwopark19 <sangwopark19@gmail.com>
2025-07-24 11:53:02 +0900

    feat:cors 매핑 설정

--- src/main/java/com/farmtastic/farm/config/WebConfig.java
+++ src/main/java/com/farmtastic/farm/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
             .allowedOriginPatterns("*") // 모든 출처의 요청을 허용
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
             .allowedHeaders("*")
